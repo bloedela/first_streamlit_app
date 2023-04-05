d46e1f00bc40b60c28303fc3d3bedc4df4fc9ad9
Adam Bloedel <34923008+bloedela@users.noreply.github.com>
2023-04-04 21:43:39 -0500

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 #Modify Dataframes
-my_fruit_list.setindex('Fruit')
+my_fruit_list.set_index('Fruit')
 
 #App Code
 st.title('My Parents New Healthy Diner')
