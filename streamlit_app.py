cee9e0855532dcc147a303e87be5dea03f0089ba
Adam Bloedel <34923008+bloedela@users.noreply.github.com>
2023-04-04 20:57:00 -0500

    Update streamlit_app.py
    add smoothie information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
+#Load Files
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+
+#App Code
 st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
@@ -9,3 +16,4 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.dataframe(my_fruit_list)
