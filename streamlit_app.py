a110df583a7031dc6c4ab6b78db086454d76e847
Adam Bloedel <34923008+bloedela@users.noreply.github.com>
2023-04-04 21:42:45 -0500

    Update streamlit_app.py
    change index of dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
+#Modify Dataframes
+my_fruit_list.setindex('Fruit')
 
 #App Code
 st.title('My Parents New Healthy Diner')
