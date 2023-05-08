e360f491a79b1711fd66ab4e3dc3a7a7340e0051
Adam Bloedel <34923008+bloedela@users.noreply.github.com>
2023-05-08 15:27:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 #Load Files
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 
 #Modify Dataframes
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -29,7 +29,11 @@ if fruits_selected == []:
 st.dataframe(my_fruit_list.loc[fruits_selected])
 
 st.header("Fruityvice Fruit Advice!")
-st.text(fruityvice_response)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
